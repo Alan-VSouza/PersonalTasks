5521496b24e2d0033729f021e4a40cd1d731d476
Alan-VSouza <alansouzeesi@gmail.com>
2025-05-20 14:16:43 -0300

    feat(MainActivity): Setup basic task listing screen

--- app/src/main/java/com/alansouza/personaltasks/MainActivity.kt
+++ app/src/main/java/com/alansouza/personaltasks/MainActivity.kt
@@ -1,20 +1,59 @@
 package com.alansouza.personaltasks
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.alansouza.personaltasks.adapter.TaskAdapter
+import com.alansouza.personaltasks.data.AppDatabase
+import com.alansouza.personaltasks.data.TaskDao
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerViewTasks: RecyclerView
+    private lateinit var taskAdapter: TaskAdapter
+    private lateinit var taskDao: TaskDao
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Simplify"
+
+
+        val rootLayout = findViewById<View>(R.id.main_container)
+        ViewCompat.setOnApplyWindowInsetsListener(rootLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        taskDao = AppDatabase.getDatabase(applicationContext).taskDao()
+
+        recyclerViewTasks = findViewById(R.id.recyclerViewTasks)
+        recyclerViewTasks.layoutManager = LinearLayoutManager(this)
+
+        taskAdapter = TaskAdapter()
+        recyclerViewTasks.adapter = taskAdapter
+
+        loadAndObserveTasks()
     }
+
+    private fun loadAndObserveTasks() {
+        taskDao.getAllTasks().observe(this, Observer { tasks ->
+            tasks?.let {
+                taskAdapter.submitList(it)
+            }
+        })
+    }
+
 }
